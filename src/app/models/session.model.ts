0ceba27d8f9b4140a3219fad065f3d964c9bab9a
[Emilijus Pocius] <[emilijus.pocius@visma.com]>
2026-02-24 10:10:51 +0200

    Create session.model.ts and generate environments

---
+++ src/app/models/session.model.ts
@@ -0,0 +1,19 @@
+
+export interface Session {
+  id: number;
+  title: string;
+  abstract?: string;
+  startTime?: string;
+  endTime?: string;
+  trackId?: number;
+}
+
+export interface CreateSession {
+  title: string;
+  abstract?: string;
+  startTime?: string;
+  endTime?: string;
+  trackId?: number;
+}
+
+export type FormState = 'idle' | 'loading' | 'success' | 'error';
